dfd39581e0edbaf73ecd26e1f2df99a1c0b21980
rostom44 <blackgamerdv7@gmail.com>
2025-06-13 01:35:58 +0200

    basic strucutre for navigation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "../styles/navbar.css";
 
 function navbar() {
-  return <nav>nav</nav>;
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" end>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/services">Service</NavLink>
+          </li>
+          {/* <li>
+            <NavLink to="/about">About</NavLink>
+          </li> */}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
         element: <Services />,
       },
       // {
-      //   path: "/about",
+      //   path: "/a propos",
       //   element: ,
       // },
       // {
