e7f52e2bfe864f8616a12ffcace41242c68f03cf
rostom44 <blackgamerdv7@gmail.com>
2025-06-12 18:37:08 +0200

    added react router and linked pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 import "./styles/App.css";
 import Navbar from "./components/navbar.jsx";
 
@@ -5,7 +7,13 @@ function App() {
   return (
     <>
       <Navbar />
-      <h1>KONET</h1>
+      <Outlet />
+      <footer>
+        <p> {new Date().getFullYear()} konet</p>
+        <p>
+          <a href="mailto:K7G7L@example.com">K7G7L@example.com</a>
+        </p>
+      </footer>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/root.css";
 import App from "./App.jsx";
+import Home from "./pages/home.jsx";
+import Services from "./pages/services.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    // errorElement: <div>Page not found</div>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+      // {
+      //   path: "/about",
+      //   element: ,
+      // },
+      // {
+      //   path: "/contact",
+      //   element: ,
+      // },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} basename="./" />
   </StrictMode>
 );
