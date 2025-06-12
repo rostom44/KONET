2024e0046a5e3ab7ca9eefda8bbc45508730ea53
rostom44 <blackgamerdv7@gmail.com>
2025-06-12 18:17:53 +0200

    added rfce to every component

---
+++ src/pages/aboutUs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function aboutUs() {
+  return <div></div>;
+}
+
+export default aboutUs;
---
+++ src/pages/services.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function services() {
+  return <div></div>;
+}
+
+export default services;
