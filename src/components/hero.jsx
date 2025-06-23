9ff61f2e85f02e146117e0932246c439b2d6aa13
rostom44 <blackgamerdv7@gmail.com>
2025-06-23 10:38:09 +0200

    added the strucutre for hero component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-function hero() {
-  return <div></div>;
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero-overlay" />
+      <img className="hero-img" src="/assets/clean1.jpg" alt="cleaning" />
+      <div className="hero-content">
+        <h1>
+          fresh,
+          <br />
+          propre
+          <br />
+          et brillant
+        </h1>
+        <p>Notre Priorité Votre Propreté</p>
+        <div className="hero-buttons">
+          <button>services</button>
+          <button>contact us</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default hero;
+export default Hero;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "../components/hero";
+
+import "../styles/home.css";
 
 function home() {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
 
 export default home;
