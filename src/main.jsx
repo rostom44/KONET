bd50eb9f53d37445e00172658029806317c293f0
rostom44 <blackgamerdv7@gmail.com>
2025-07-18 21:56:03 +0200

    added 404 not found page

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/root.css";
+
 import App from "./App.jsx";
 import Home from "./pages/home.jsx";
 import Services from "./pages/services.jsx";
 import About from "./pages/about.jsx";
 import Contact from "./pages/contact.jsx";
+import ErrorPage from "./pages/error.jsx";
 
 const router = createBrowserRouter([
   {
     element: <App />,
-    // errorElement: <div>Page not found</div>,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
